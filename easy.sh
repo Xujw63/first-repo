77ed36447c730a83db414308cf500df04a9529a8
Xujw63 <xuj663332@163.com>
2023-10-23 23:44:13 +0800

    Implement the function to exit the shell

--- easy.sh
+++ easy.sh
@@ -1,7 +1,19 @@
 import cmd
+import shlex                                                        
 
 class PyShell(cmd.Cmd()):
-    pass
+    def do_exit(self,arg_str:str) -> None:
+        """usage: exit [exitcode]"""
+        args=shlex.split(arg_str)
+        if len(args)>1:
+            print('exit:too many arguments',flie=sys.stderr)
+            return
+        if len(args)==0 or arg[0]=='':
+            exit(0)
+        try:
+            exit(int(args[0]))
+        except ValueError:
+            print('exit:invalid exit code',file=sys.stderr) 
     
 if __name__=='main':
     cmd.Cmd().cmdloop()
